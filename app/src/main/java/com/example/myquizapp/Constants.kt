98051a39586c0f56a894653dc06bb096b68bfd63
net6825 <net6825@naver.com>
2022-06-13 20:47:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myquizapp/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.myquizapp
+
+object Constants {
+
+//    fun getQuestions():ArrayList<Question>{
+//        val questionList = ArrayList<Question>()
+
+//        val que1 = Question(1, "What country does this flag belong to?", R.drawable.ic_bg, )  //이미지 Int로 사용 가능
+
+//        questionList.add()
+//    }
+}
