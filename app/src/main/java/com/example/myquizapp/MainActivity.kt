045e8b0ef3f0dba19362098c3932f42388e59c97
net6825 <net6825@naver.com>
2022-06-16 17:58:02 +0900

    MyQuizApp Complete

--- app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -6,21 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonStart: Button = findViewById(R.id.btn_start)
+        val etName: AppCompatEditText = findViewById(R.id.et_name)
 
-        val btnStart: Button = findViewById(R.id.btn_start)
-        val etName: EditText = findViewById(R.id.et_name)
-        btnStart.setOnClickListener {
-            if (etName.text.isEmpty()) {
-                Toast.makeText(this, "없음", Toast.LENGTH_LONG).show()
-            }else {
-                val intent = Intent(this, QuizQuestionActivity::class.java)
+        buttonStart.setOnClickListener {
+            if (etName.text.toString().isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.toString())
                 startActivity(intent)
-                finish()
             }
         }
     }
